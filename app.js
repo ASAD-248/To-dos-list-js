040aa9e4a3cdcc0068f7c2c3ee88492a5914a453
ASAD ANSARI <mdasadansari11223@gmail.com>
2025-12-31 09:46:52 -0800

    new created file updated

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+let allTsks = [];
+console.log(allTsks);
+
+let userInput = document.getElementById("inp");
+
+let task = {
+    takTittle: '',
+    createdDate: '',
+    start: '',
+    extendDate:'',
+    delete(){}
+}
+function taskAdd(){
+// let inpValue = userInput.value.trim();
+    if (userInput.value !== '') {
+    allTsks.push(userInput.value);
+    userInput.value = '';
+    userInput.focus();
+    console.log(userInput.value);
+    console.log(allTsks);    
+    }else{
+        alert("Plese enter the task here")
+    } 
+};
+
+
